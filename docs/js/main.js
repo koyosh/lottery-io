c97a2f3a9975dff2c33d4d28d1770f0cd14379cc
koyoshida <ko.yosh95@gmail.com>
2022-12-25 07:14:08 +0900

    Function developed

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,12 @@
 const fadeInElements = document.querySelectorAll('.before-fade-in-upwards');
-console.log(fadeInElements);
+const addInput = document.querySelector('#add-wrapper > input');
+const addNumber = document.querySelector('#add-wrapper > input:nth-child(2)');
+const addButton = document.querySelector('#add-wrapper > button');
+const execButton = document.querySelector('#exec-button');
+const optionWrapper = document.querySelector('#option-wrapper');
+const winnerWindow = document.querySelector('#winner-window');
+const winnerDisplay = document.querySelector('#winner-window > h1');
+const closeWinnerWindowButton = document.querySelector('#winner-window > button');
 
 fadeInUpwards = (target) => {
   target.classList.remove('hide');
@@ -9,9 +16,70 @@ fadeInUpwards = (target) => {
   }, 1500)
 }
 
-onload = setTimeout(() => {
-  fadeInElements.forEach((e) => {
-    console.log(e);
-    fadeInUpwards(e);
+addOption = () => {
+  if (addInput.value) {
+    let number = addNumber.value;
+    if (number == '') number = 1;
+    for (let i = 0; i < number; i++) {
+      const optionDiv = document.createElement('div');
+      optionDiv.classList.add('options');
+      optionDiv.innerText = addInput.value;
+      optionDiv.addEventListener('click', () => {
+        optionDiv.remove();
+      })
+      optionWrapper.append(optionDiv);
+    }
+    addInput.value = '';
+    addNumber.value = '';
+    addInput.focus();
+  } else {
+    alert('You need to enter some value in the field first.');
+  }
+}
+
+executeLottery = () => {
+  const options = document.querySelectorAll('.options');
+  if (options.length >= 2) {
+    let rand = Math.random();
+    let key = Math.floor(rand * options.length);
+    let winner = options[key].innerText;
+    winnerDisplay.innerText = '　';
+    winnerWindow.classList.remove('hide');
+    setTimeout(()=>{
+      winnerDisplay.innerText = winner;
+    }, 1500);
+  } else {
+    alert('You need at least two options to do the lottery.');
+  }
+}
+
+eventListenerReady = () => {
+  addButton.addEventListener('click', addOption);
+  setTimeout(()=>{
+    addInput.focus();
+  }, 2000)
+  execButton.addEventListener('click', executeLottery);
+  closeWinnerWindowButton.addEventListener('click', () => {
+    winnerWindow.classList.add('hide');
   })
-}, 1500);
+}
+
+onload = () => {
+  setTimeout(() => {
+    fadeInElements.forEach((e) => {
+      fadeInUpwards(e);
+    })
+  }, 1500);
+  eventListenerReady();
+}
+
+shortcut.add('enter', () => {
+  if (document.activeElement == addInput || document.activeElement == addNumber) {
+    console.log('Enter key pressed.')
+    addOption();
+  }
+});
+
+shortcut.add('meta+enter', ()=>{
+    executeLottery();
+})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/docs`));
 
 app.get(':file', (req, res) => {
   const file = req.params.file;
